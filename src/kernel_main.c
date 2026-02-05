69b5245fd7c6a24d4a35f9b087d31b173551ebfd
calvinkallio1 <ckallio@luc.edu>
2026-02-05 12:12:00 +0000

    Changes to be committed: 	modified:   Makefile 	modified:   grub.cfg 	modified:   src/kernel_main.c

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,7 +1,11 @@
-
 #include <stdint.h>
+#include "putc.h"
 
 #define MULTIBOOT2_HEADER_MAGIC         0xe85250d6
+
+void putc(int data);
+void scroll(void);
+void clear(int rowToClear);
 
 const unsigned int multiboot_header[]  __attribute__((section(".multiboot"))) = {MULTIBOOT2_HEADER_MAGIC, 0, 16, -(16+MULTIBOOT2_HEADER_MAGIC), 0, 12};
 
@@ -11,15 +15,12 @@ uint8_t inb (uint16_t _port) {
     return rv;
 }
 
-void main() {
-    unsigned short *vram = (unsigned short*)0xb8000; // Base address of video mem
-    const unsigned char color = 7; // gray text on black background
+void outb(uint16_t _port, uint8_t val){
+	__asm__ __volatile__ ("outb %0, %1" :: "a" (val), "dN" (_port));
+}
 
-    while(1) {
-        uint8_t status = inb(0x64);
-
-        if(status & 1) {
-            uint8_t scancode = inb(0x60);
-        }
-    }
+void main() {	
+	
+	putc('a');
+	while(1);
 }
