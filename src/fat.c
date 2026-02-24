6b58c0b5f07e51e19a6561569cb05f1711a16fbd
calvinkallio1 <ckallio@luc.edu>
2026-02-24 20:15:21 +0000

    Changes to be committed: 	modified:   src/fat.c  Untracked files: 	src/fatdriver.c

--- src/fat.c
+++ src/fat.c
@@ -1,10 +1,35 @@
 #include <stdint.h>
 #include "fat.h"
+#include "ide.h"
 
-int fatInit(){
+int fd = 0;
 
-	
+void driver_init(char *path_to_file){
+
+   fd = ata_lba_read(0, path_to_file, 4); 
 
 }
 
 
+void sector_read(unsigned int sector_num, char *buf){
+
+    
+
+}
+
+int fatInit(){
+
+	
+}
+
+struct rde * fatOpen(char *path){
+
+    
+
+}
+
+int fatRead(struct rde *rde, char *buf, int n){
+
+
+
+}
